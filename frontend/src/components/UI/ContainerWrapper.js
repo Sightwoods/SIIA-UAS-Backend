b8475f6f5188a91a95eb6176be275b95d65f31b2
Osiris Meza <81686033+OsirisMH@users.noreply.github.com>
2021-12-29 23:49:33 -0700

    Delete frontend directory

--- frontend/src/components/UI/ContainerWrapper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const ContainerWrapper = (props) => {
-    return (
-        <div>
-            { props.children }
-        </div>
-    )
-}
--- frontend/src/components/layout/Main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Main = (props) => {
-    return (
-        <main>
-            { props.children }
-        </main>
-    )
-}
